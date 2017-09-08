204ed63bb55355e6e0eb0d1946a63020b213f4c4
Gordon Forsythe <baohx2000@gmail.com>
2017-09-08 11:47:05 -0700

    add children propType

--- src/OfflineComponent.js
+++ src/OfflineComponent.js
@@ -1,6 +1,11 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class OfflineComponent extends Component {
+  static propTypes = {
+    children: PropTypes.func.isRequired,
+  };
+
   state = {
     isOnline: window.navigator.onLine,
   };
